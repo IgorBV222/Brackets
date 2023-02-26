3c86e48a5b26af808c4424d6c0a36dcbde14262c
Игорь <@DESKTOP-URMNCOP>
2023-02-26 20:42:57 +0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+
+	std::string str;
+	int counterBrackets = 0;
+	int counterSymbols = 0;
+	std::vector <int>  vecSymbols;
+	
+	std::cout << "Enter a line with brackets (): \nenter -> ";
+	getline(std::cin, str);
+
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == '(')
+			counterBrackets++;
+		else if (str[i] == ')') {
+			vecSymbols.push_back(counterSymbols);
+			counterSymbols = 0;
+		}
+		else
+			counterSymbols++;
+	}
+
+	std::cout << "The number of pairs of brackets = " << counterBrackets << '\n';
+	std::cout << "The number of symbols = " << counterSymbols << '\n';
+
+	for (auto el : vecSymbols)
+		std::cout << el << ' ';
+
+	return 0;
+}
